52aa1a7252019f7c0c80c6ad396a0bbcfb616a8d
Rizvan555 <rizvan84@gmx.de>
2023-03-22 09:33:45 +0100

    added new function to 2_2

--- lvl_2_2/assets/js/main.js
+++ lvl_2_2/assets/js/main.js
@@ -1,2 +1,19 @@
 let board = document.querySelector("#board");
-let buttons = document.querySelectorAll(".button");
+let button1 = document.querySelector(".button1");
+let button2 = document.querySelector(".button2");
+let button3 = document.querySelector(".button3");
+const keySpan = document.querySelector(".button-box1 span");
+const keyCodeSpan = document.querySelector(".button-box2 span");
+const codeSpan = document.querySelector(".button-box3 span");
+
+button1.addEventListener("click", (event) => {
+  board.innerHTML = keySpan.textContent;
+});
+
+button2.addEventListener("click", (event) => {
+  board.innerHTML = keyCodeSpan.textContent;
+});
+
+button3.addEventListener("click", (event) => {
+  board.innerHTML = codeSpan.textContent;
+});
